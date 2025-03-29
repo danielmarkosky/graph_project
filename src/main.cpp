f8f320770aa846ba4931d88724625bcbc5338d6e
markoskydaniel <markoskydaniel@gmail.com>
2025-03-29 21:58:03 +0100

    adding sandbox

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ int main() {
     graph.iterate();
   }
   if (iter == max_iterations + 1) {
-    std::cout << "Graph is connected after " << max_iterations << " iterations." << std::endl;
+    std::cout << "Graph is still connected after " << max_iterations << " iterations." << std::endl;
   } else {
     std::cout << "Graph is not connected after " << iter << " iterations." << std::endl;
   }
